b6b9e9337f18939e6866ef39ac92653912b8bd81
Aleks <Allexa678@gmail.com>
2020-02-27 19:00:30 +0300

    Реализует перемещение диалога редактирования персонажа

--- js/dialog.js
+++ js/dialog.js
@@ -6,8 +6,8 @@
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var setupWizard = document.querySelector('.setup-wizard');
   var userSetup = document.querySelector('.setup');
+  var setupWizard = document.querySelector('.setup-wizard');
   var setupOpen = document.querySelector('.setup-open');
   var wizardCoat = setupWizard.querySelector('.wizard-coat');
   var inputCoat = userSetup.querySelector('[name=coat-color]');
